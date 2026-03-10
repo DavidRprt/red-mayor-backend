05ea951622e21a2f6a58cc13e1963a4e9a74a352
David <davirapo@gmail.com>
2026-03-10 14:12:55 -0300

    Actualizando subida de fotos

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -30,7 +30,17 @@ module.exports = [
   },
   "strapi::poweredBy",
   "strapi::query",
-  "strapi::body",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "10mb",
+      jsonLimit: "10mb",
+      textLimit: "10mb",
+      formidable: {
+        maxFileSize: 10 * 1024 * 1024, // 10mb
+      },
+    },
+  },
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
